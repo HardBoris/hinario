ec0e7e2190a80181d5e281eebaae4c514c2a0b43
HardBoris <bgaibor03@gmail.com>
2022-10-04 19:31:12 -0400

    feat: initial migration

---
+++ src/migrations/1664924503381-initialMig.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class initialMig1664924503381 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
