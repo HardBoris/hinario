1d47d51cc1dcfafa27019afa2137fa0cc390f13a
HardBoris <bgaibor03@gmail.com>
2022-11-20 11:56:24 -0400

    feat: object hymn

--- src/entities/Hymn.ts
+++ src/entities/Hymn.ts
@@ -1,4 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { AppDataSource } from "../data-source";
+// import hymnal100 from "../hinario/hymnal100.json"
 
 @Entity("hymns")
 export class Hymn {
@@ -19,4 +21,13 @@ export class Hymn {
 
   @Column({ nullable: true })
   originalTitle?: string;
+
+  inserta = async () =>
+    await AppDataSource.createQueryBuilder()
+      .insert()
+      .into("hymns")
+      .values([
+        { hymnId: "prueba", hymnNumber: "456", hymnTitle: "titulo de prueba" },
+      ])
+      .execute();
 }
