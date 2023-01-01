b785fd5095ae90bbf1dcbe863127110182b2da96
HardBoris <bgaibor03@gmail.com>
2023-01-01 14:52:21 -0400

    feat: app refatored

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import "express-async-errors";
 import express, { NextFunction, Request, Response } from "express";
 import { errorHandler } from "./errors";
-
+import cors from "cors";
 import registerRouters from "./routes";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 registerRouters(app);
 
