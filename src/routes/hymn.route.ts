1456b8895ad7117fa9156bc25c4e22b05183a384
HardBoris <bgaibor03@gmail.com>
2023-02-23 21:05:15 -0400

    feat: hymn route refatored

--- src/routes/hymn.route.ts
+++ src/routes/hymn.route.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import hymnController from "../controllers/hymn.controller";
-import playerValidator from "../middlewares/playerValidator.middleware";
-import tokenValidator from "../middlewares/tokenValidator.middleware";
+// import playerValidator from "../middlewares/playerValidator.middleware";
+// import tokenValidator from "../middlewares/tokenValidator.middleware";
 
 const hymnRouter = Router();
 
 hymnRouter.get(
   "/hymns",
-  tokenValidator,
-  playerValidator,
+  // tokenValidator,
+  // playerValidator,
   hymnController.hymnLoader
 );
 
